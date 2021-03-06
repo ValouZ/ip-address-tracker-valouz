9faf9bbb34cca1d2bf5d28cd250b0ec1cadfdf72
ValouZ <57949313+ValouZ@users.noreply.github.com>
2021-03-06 18:34:38 +0100

    Delete .nuxt directory

--- .nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as AddressCard } from '../..\\components\\AddressCard.vue'
-export { default as AddressInput } from '../..\\components\\AddressInput.vue'
-export { default as Header } from '../..\\components\\Header.vue'
-export { default as Map } from '../..\\components\\Map.vue'
-
-export const LazyAddressCard = import('../..\\components\\AddressCard.vue' /* webpackChunkName: "components/address-card" */).then(c => c.default || c)
-export const LazyAddressInput = import('../..\\components\\AddressInput.vue' /* webpackChunkName: "components/address-input" */).then(c => c.default || c)
-export const LazyHeader = import('../..\\components\\Header.vue' /* webpackChunkName: "components/header" */).then(c => c.default || c)
-export const LazyMap = import('../..\\components\\Map.vue' /* webpackChunkName: "components/map" */).then(c => c.default || c)
